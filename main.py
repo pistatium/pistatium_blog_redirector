4ba1b6c0d01665f6130e2c33ebb828a2a2212b3f
kimihiro_n <394378+pistatium@users.noreply.github.com>
2021-12-25 20:05:10 +0900

    Add logger

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
+import logging
 
-from flask import Flask
+from flask import Flask, request
 
 
 TO_REDIRECT = os.environ.get('TO_REDIRECT')
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger('redirector')
 
 app = Flask(__name__)
 
@@ -11,6 +15,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def redirector(path: str):
+    logger.info(f"ua: {request.headers.get('User-Agent')}")
     headers = {}
     headers['Cache-Control'] = 'public, max-age=1209600'   
     headers['Location'] = f'{TO_REDIRECT}/{path}'
